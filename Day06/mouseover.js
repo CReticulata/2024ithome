3f51a8652ca1971b6e882299bce565de829e247d
CC Tung <maple51327@gmail.com>
2024-09-19 10:05:16 +0800

    feat: day06

---
+++ Day06/mouseover.js
@@ -0,0 +1,14 @@
+const circleBlue = document.querySelector(".circle-1");
+const circleYellow = document.querySelector(".circle-2");
+const mouseLocation = document.querySelector(".location");
+
+function showLocation() {
+  const location = this.attributes.name.value;
+
+  mouseLocation.textContent = `鼠輩位置在${location}`;
+
+  console.log(`鼠輩位置在${location}`);
+}
+
+circleBlue.addEventListener("mouseover", showLocation);
+circleYellow.addEventListener("mouseover", showLocation);
